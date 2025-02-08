a4f94360998807ba4d1f611e53aba537b0f86f82
hank <apple84026113@gmail.com>
2025-02-08 16:27:55 +0800

    update data emoji

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -286,7 +286,7 @@ def main():
                     if filtered_tables:
                         for key, table in filtered_tables.items():
                             with st.container(border=True):
-                                st.subheader(🔹 + key)
+                                st.subheader("🔹 "+ key)
                             # with st.expander(key, expanded=True):
                                 # 主項資料
                                 # st.markdown("### 主項")
